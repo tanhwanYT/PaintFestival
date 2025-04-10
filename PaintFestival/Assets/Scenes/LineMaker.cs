7b09f614deb07cd16985ef9d906fd5c44f52daeb
tanhwanYT <154482081+tanhwanYT@users.noreply.github.com>
2025-04-10 11:09:44 +0900

    2025 04 10 라인 메이커
    ㅈㄱㄴ

---
+++ PaintFestival/Assets/Scenes/LineMaker.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class LineMaker : MonoBehaviour
+{
+    public GameObject linePrefab;
+
+    LineRenderer lr;
+    EdgeCollider2D col;
+    List<Vector2> points  = new List<Vector2>();
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject go = Instantiate(linePrefab);
+            lr = go.GetComponent<LineRenderer>();
+            col = go.GetComponent<EdgeCollider2D>();  
+            points.Add(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+            lr.positionCount = 1;
+            lr.SetPosition(0, points[0]);
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            Vector2 pos  = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            if (Vector2.Distance(points[points.Count - 1], pos) > 0.1f)
+            {
+                points.Add(pos);
+                lr.positionCount++;
+                lr.SetPosition(lr.positionCount - 1, pos);
+                col.points = points.ToArray();
+            }
+        }
+        else if(Input.GetMouseButtonUp(0))
+        {
+            points.Clear();
+        }
+    }
+}

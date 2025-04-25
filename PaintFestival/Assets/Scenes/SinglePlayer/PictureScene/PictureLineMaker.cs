4934a07fa3a613a8115708c91204a5a82870bd6b
tanhwanYT <154482081+tanhwanYT@users.noreply.github.com>
2025-04-25 13:26:58 +0900

    2025 04 25 싱글플레이 그림판 업그레이드
    ctrl + z 기능 제작
    그외에 있던 버그들 전부 수정

--- PaintFestival/Assets/Scenes/SinglePlayer/PictureScene/PictureLineMaker.cs
+++ PaintFestival/Assets/Scenes/SinglePlayer/PictureScene/PictureLineMaker.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PictureLineMaker : MonoBehaviour
 {
@@ -9,6 +11,8 @@ public class PictureLineMaker : MonoBehaviour
     private GameObject PaletteManager;
     private PaletteManager palette;
     private Color currentColor = Color.black;
+    private int lineSortingOrder = 0;
+    private List<GameObject> lines = new List<GameObject>();
 
     LineRenderer lr;
     List<Vector2> points = new List<Vector2>();
@@ -21,35 +25,61 @@ public class PictureLineMaker : MonoBehaviour
         palette.OnColorSelected.AddListener(OnColorChanged);
     }
     // Update is called once per frame
+
     void Update()
     {
+        if (EventSystem.current.IsPointerOverGameObject())
+        {
+            return;
+        }
+
         if (Input.GetMouseButtonDown(0))
         {
+            Vector2 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(mouseWorldPos, Vector2.zero);
+
+            if (hit.collider != null && hit.collider.CompareTag("Button"))
+            {
+                return;
+            }
+
             GameObject go = Instantiate(linePrefab);
+            lines.Add(go);
 
             lr = go.GetComponent<LineRenderer>();
-
             lr.material.color = currentColor;
+            lr.sortingOrder = ++lineSortingOrder;
 
-            points.Add(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePos.z = 0f; 
+
+            points.Add(mousePos);
             lr.positionCount = 1;
-            lr.SetPosition(0, points[0]);
+            lr.SetPosition(0, mousePos);
         }
         else if (Input.GetMouseButton(0))
         {
-            Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            if (points.Count > 0 && Vector2.Distance(points[points.Count - 1], pos) > 0.1f)
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePos.z = 0f;
+
+            if (points.Count > 0 && Vector2.Distance(points[points.Count - 1], mousePos) > 0.1f)
             {
-                points.Add(pos);
+                points.Add(mousePos);
                 lr.positionCount++;
-                lr.SetPosition(lr.positionCount - 1, pos);
+                lr.SetPosition(lr.positionCount - 1, mousePos);
             }
         }
         else if (Input.GetMouseButtonUp(0))
         {
             points.Clear();
         }
+
+        if (Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.Z))
+        {
+            UndoLastLine();
+        }
     }
+
     private void OnColorChanged(Color newColor)
     {
         currentColor = newColor;
@@ -57,5 +87,15 @@ public class PictureLineMaker : MonoBehaviour
     private void OnDestroy()
     {
         palette.OnColorSelected.RemoveListener(OnColorChanged);
+    }
+    private void UndoLastLine()
+    {
+        if(lines.Count > 0)
+        {
+            GameObject LastLine = lines[lines.Count - 1];
+            lines.RemoveAt(lines.Count - 1);
+            Destroy(LastLine);
+            lineSortingOrder--;
+        }
     }
 }

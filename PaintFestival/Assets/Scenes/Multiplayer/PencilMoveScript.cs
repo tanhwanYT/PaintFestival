753d4871bfaf2c1db8f3bf3d3eb52e57f163c18e
tanhwanYT <154482081+tanhwanYT@users.noreply.github.com>
2025-05-08 22:34:38 +0900

    2025 05 08 멀티플레이 마우스 업그레이드
    마우스포인터 UI에서 안 보이게 한 후 연필이 따라다니게 함 상당히 게임성이 좋아짐

---
+++ PaintFestival/Assets/Scenes/Multiplayer/PencilMoveScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Tilemaps;
+using UnityEngine;
+
+public class PencilMoveScript : MonoBehaviour
+{
+    public float pencilXpos;
+    public float pencilYpos;
+    private void Start()
+    {
+        Cursor.visible = false;
+    }
+    void Update()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 offset = new Vector2(pencilXpos, pencilYpos);
+        transform.position = mousePos + offset;
+    }
+}
